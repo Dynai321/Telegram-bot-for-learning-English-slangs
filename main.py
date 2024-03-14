c6c0d4fd4b5c0a348a3ceff05e8b28c77f0ab15b
dsgfs <dsgfsdhfgjkghk@mail.ru>
2024-03-15 01:49:35 +0300

    add main.py

--- main.py
+++ main.py
@@ -1,35 +1,57 @@
 import telebot
 from telebot import types
+import psycopg2
 
 bot = telebot.TeleBot('6663315211:AAFIdDTm0iief_r901XzICqvdh2wYKdcjzg')
+
+conn = psycopg2.connect(
+    dbname='TelegramBotDB',
+    user='postgres',
+    password='1234',
+    host='localhost',
+    port='5432'
+)
+cursor = conn.cursor()
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
+    cursor.execute('SELECT * FROM users WHERE id=%s', (message.from_user.id,))
+    user = cursor.fetchone()
 
+    if not user:
+        cursor.execute('INSERT INTO users (id, username) VALUES (%s, %s)',
+                       (message.from_user.id, message.from_user.username))
+        conn.commit()
+
+    keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
     button_word = types.KeyboardButton('Слово')
     button_learned = types.KeyboardButton('Выучено')
     button_my_rating = types.KeyboardButton('Мой рейтинг')
     button_dictionary = types.KeyboardButton('Словарь')
     button_help = types.KeyboardButton('Помощь')
-
     keyboard.add(button_word, button_learned, button_my_rating, button_dictionary, button_help)
-
     bot.send_message(message.chat.id, "Привет, {}! Нажми кнопку.".format(message.from_user.first_name),
                      reply_markup=keyboard)
 
 
-# Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
 def text(message):
     if message.text == 'Помощь':
-        bot.send_message(message.chat.id, "1) Кнопка 'Cлово' выводит новое слово для изучения;\n"
+        bot.send_message(message.chat.id, "1) Кнопка 'Слово' выводит новое слово для изучения;\n"
                                           "2) Кнопка 'Выучено' выводит выученные слова;\n"
                                           "3) Кнопка 'Мой рейтинг' выводит рейтинг в котором ведется счет выученных "
                                           "слов после чего выстраивайтся рейтинг лучших учеников;\n"
                                           "4) Кнопка 'Словарь' выводит словарь всех слов;")
+    elif message.text == 'Слово':
+        cursor.execute('SELECT slang_word, definition FROM words ORDER BY RANDOM() LIMIT 1')
+        result = cursor.fetchone()
+        if result:
+            slang_word, definition = result
+            bot.send_message(message.chat.id, f"Слово: {slang_word}\nОписание: {definition}")
 
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
+
+conn.close()
